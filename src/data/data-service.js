2bd17a150fabe34b4a8fa0ad12be6cccd4e904b5
Ron Apelbaum <ronapelbaum@gmail.com>
2017-03-21 08:01:29 +0200

    add data-service.js

---
+++ src/data/data-service.js
@@ -0,0 +1,6 @@
+class DataService{
+    add(item){
+        console.log('ron data-service.add()', item);//TODO remove this
+    }
+}
+export default DataService;
--- src/movies/movies-data.js
+++ src/movies/movies-data.js
@@ -1,19 +1,28 @@
 import UserData from '../user/user-data';
+import DataService from '../data/data-service';
+
 const topMovies = [
     {title: 'Ironman', year: 2008, genre: ['action', 'sci-fi']},
     {title: 'Titanic', year: 1997, genre: ['drama']}
 ];
 
-class MoviesData{
-    constructor(){
+class MoviesData {
+    constructor() {
         this.ud = new UserData();
+        this.ds = new DataService();
     }
-    getMoreMovies(){
+
+    getMoreMovies() {
         return topMovies;
     }
 
-    vote(movie, vote){
+    vote(movie, vote) {
         console.log('ron movies-data.vote()', this.ud.getUserId(), movie.title, vote);//TODO remove this
+        this.ds.add({
+            userId: this.ud.getUserId(),
+            movie: movie,
+            vote: vote
+        })
     }
 }
 export default MoviesData;
