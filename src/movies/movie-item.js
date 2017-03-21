e288b4d495417de4caa59698a3ebcbeea7de85a5
Ron Apelbaum <ronapelbaum@gmail.com>
2017-03-21 07:38:40 +0200

    add voting buttons

--- src/movies/movie-item.js
+++ src/movies/movie-item.js
@@ -4,14 +4,36 @@ class MovieItem extends Component {
     constructor(props) {
         super(props);
         this.movie = props.movie;
+
         console.log('ron movie-item.constructor()', this.movie);//TODO remove this
+
+        this.like = this.like.bind(this);
+        this.dislike = this.dislike.bind(this);
+        this.saveForLater = this.saveForLater.bind(this);
+    }
+
+    like() {
+        console.log('ron movie-item.like()', this.movie.title);//TODO remove this
+    }
+
+    dislike() {
+        console.log('ron movie-item.dislike()', this.movie.title);//TODO remove this
+    }
+
+    saveForLater() {
+        console.log('ron movie-item.saveForLater()', this.movie.title);//TODO remove this
     }
 
     render() {
         return (
             <div>
                 <h3>{this.movie.title} ({this.movie.year})</h3>
-                <span>{this.movie.genre}</span>
+                <span>{this.movie.genre.join(',')}</span>
+                <div>
+                    <button onClick={this.dislike}>Dislike</button>
+                    <button onClick={this.saveForLater}>Save For Later</button>
+                    <button onClick={this.like}>Like</button>
+                </div>
             </div>
         );
     }
