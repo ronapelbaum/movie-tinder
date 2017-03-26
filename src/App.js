b398e318e985c6b656c61ab168be1e3e9f81d9a9
Ron Apelbaum <ronapelbaum@gmail.com>
2017-03-26 13:44:47 +0300

    add container

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     return (
       <div className="text-center">
           <Navbar title={this.title}/>
-          <MoviesList/>
+          <div className="container">
+              <MoviesList/>
+          </div>
       </div>
     );
   }
