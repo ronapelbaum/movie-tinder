3d87477cdf40a583f4f4cb7645f928b9131bae64
Ron Apelbaum <ronapelbaum@gmail.com>
2017-03-26 13:09:57 +0300

    remove IconButton

--- src/utils/ui.js
+++ src/utils/ui.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 export const Icon = (props) => <i className={'fa fa-' + props.icon}></i>;
 
-export const Button = (props) => <button className="btn btn-default" onClick={props.onClick}>{props.children}</button>;
-
-export const IconButton = (props) => <Button onClick={props.onClick}><Icon icon={props.icon}/></Button>;
+export const Button = (props) => {
+    let content = props.icon ? <Icon icon={props.icon}/> : props.children;
+    return <button className="btn btn-default" onClick={props.onClick}>{content}</button>;
+};
 
 export const ButtonGroup = (props) => {
-    const buttons = props.buttons.map((props) => <IconButton onClick={props.onClick} icon={props.icon}/>);
+    const buttons = props.buttons.map((props, index) => <Button onClick={props.onClick} icon={props.icon} key={index}/>);
     return (<div className="btn-group">{buttons}</div>);
 };
